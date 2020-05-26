42f3fa13462448bb3ddc540ef9e65883a2e19fb4
Evgeny Tupikov <bwinter@yandex.ru>
2020-05-26 22:33:09 +0500

    Save default data

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './entities/user.entity';
-import { Message } from './entities/message.entity';
 import { AuthModule } from './auth/auth.module';
-import { MessagesModule } from './rest/messages/messages.module';
 import { UsersModule } from './rest/users/users.module';
 import { AppController } from './app.controller';
 import { config } from './config';
@@ -13,9 +11,8 @@ import { config } from './config';
 @Module({
   imports: [
     ServeStaticModule.forRoot(config.static),
-    TypeOrmModule.forRoot({...config.db, entities: [User, Message]}),
+    TypeOrmModule.forRoot({...config.db, entities: [User]}),
     AuthModule,
-    MessagesModule,
     UsersModule
   ],
   controllers: [AppController]
