0c3c70f92a7a815baff0ce49c4c28b972fc1e225
Evgeny Tupikov <bwinter@yandex.ru>
2020-12-06 01:25:00 +0500

    Built project

---
+++ backend/dist/migrations/1603013566862-create_tables.d.ts
@@ -0,0 +1,6 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+export declare class createTables1603013566862 implements MigrationInterface {
+    name: string;
+    up(queryRunner: QueryRunner): Promise<void>;
+    down(queryRunner: QueryRunner): Promise<void>;
+}
