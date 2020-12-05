55cecec15ea8d58e11e037d4e4cea5d07188c279
Evgeny Tupikov <bwinter@yandex.ru>
2020-12-06 01:31:19 +0500

    Change port and rebuilt

--- backend/dist/src/main.js
+++ backend/dist/src/main.js
@@ -6,7 +6,7 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.setGlobalPrefix('/api');
-    await app.listen(3000, '0.0.0.0');
+    await app.listen(3031, '0.0.0.0');
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
