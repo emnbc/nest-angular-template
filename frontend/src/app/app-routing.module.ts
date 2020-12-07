897eb911518b71f5e03f155f8e127456ed0200a6
Evgeny Tupikov <bwinter@yandex.ru>
2020-12-08 00:55:04 +0500

    Profile page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IsNotAuthGuard } from './guards/is-not-auth.guard';
+import { IsAuthGuard } from './guards/is-auth.guard';
 
 const routes: Routes = [
   {
     path: 'welcome',
     loadChildren: () => import('./pages/welcome-page/welcome-page.module').then(m => m.WelcomePageModule),
     canLoad: [IsNotAuthGuard]
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile-page/profile-page.module').then(m => m.ProfilePageModule),
+    canLoad: [IsAuthGuard]
   },
   {
     path: 'sign-in',
